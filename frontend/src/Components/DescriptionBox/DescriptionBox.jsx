749d0319e7ec34038f8ab0a31174fd3b04238706
cooliowilliams <Williamsobinna677@gmail.com>
2024-01-23 12:47:01 +0100

    Second commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>An E-Commerce is an online platform that facilitates the buying and selling products or services over the internet sevices as a virtual marketspace where businesses and individuals showcase their products and interact with customers, and conduct transactions without physical presence. E-Commerce website have gained immense popularity due to their convenient accessibility, and global reach they offer.</p>
+            </div>
+         </div>
+  )
+}
+
+export default DescriptionBox
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 import all_product from "../Components/Assets/all_product"
 
@@ -6,9 +6,28 @@ import all_product from "../Components/Assets/all_product"
 export const ShopContext = createContext(null);
 
 
-const ShopContextProvider = (props) => {
+    const getDefaultCart = ()=>{
+        let cart = {};
+        for (let index = 0; index < all_product.length+1; index++) {
+            cart[index] = 0;
+        }
+        return cart;
+    }
 
-    const contextValue = {all_product}
+    const ShopContextProvider = (props) => {
+        const [cartItems, setCartItems] = useState(getDefaultCart());
+        
+
+        const addToCart = (itemsId) =>{
+            setCartItems((prev)=>({...prev,[itemsId]:prev[itemsId]+1}))
+            console.log(cartItems);
+        }
+
+        const removeFromCart = (itemsId) =>{
+            setCartItems((prev)=>({...prev,[itemsId]:prev[itemsId]-1}))
+        }
+
+        const contextValue = {all_product,cartItems,addToCart,removeFromCart};
 
     return (
         <ShopContext.Provider value={contextValue}>
